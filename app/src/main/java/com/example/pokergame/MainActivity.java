c1282c7f96bdc9d5a5b46bada2e58edf9a7100dd
SantiAce26 <santiaacevedo26@gmail.com>
2021-11-18 00:01:31 -0500

    Adds assets and adds a menu interface

--- app/src/main/java/com/example/pokergame/MainActivity.java
+++ app/src/main/java/com/example/pokergame/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.pokergame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem a = menu.add("Settings");
+        MenuItem b = menu.add("Account");
+        MenuItem c = menu.add("Exit");
+        return true;
     }
 }
