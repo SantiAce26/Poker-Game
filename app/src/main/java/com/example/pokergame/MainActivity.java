1c4c4995a6664b9338a64c08b7f00b180661c9de
SantiAce26 <santiaacevedo26@gmail.com>
2021-11-19 02:29:22 -0500

    Adds a little touch to display username in main menu

--- app/src/main/java/com/example/pokergame/MainActivity.java
+++ app/src/main/java/com/example/pokergame/MainActivity.java
@@ -1,21 +1,51 @@
 package com.example.pokergame;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView userNameHolder;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userNameHolder = findViewById(R.id.welcomeMessage);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        userID = fAuth.getCurrentUser().getUid();
+
+        //Welcome message with user's name
+        DocumentReference documentReference = fStore.collection("users").document(userID);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                userNameHolder.setText("Welcome " + value.getString("username"));
+            }
+        });
+
     }
 
     public void logout(View view) {
@@ -23,6 +53,14 @@ public class MainActivity extends AppCompatActivity {
         FirebaseAuth.getInstance().signOut();
         startActivity(new Intent(getApplicationContext(), LoginActivity.class));
         finish();
+    }
+
+    public void leaveApp(View view) {
+        //Exits application
+        finish();
+        Log.d("Exit", "User exiting application");
+        finishAffinity();
+        System.exit(0);
     }
 
 }
