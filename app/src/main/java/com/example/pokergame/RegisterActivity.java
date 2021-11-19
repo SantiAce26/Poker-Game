34910e02315004570bffbeaaafc8a94eb8ba4cc1
SantiAce26 <santiaacevedo26@gmail.com>
2021-11-19 00:50:29 -0500

    Stores user information into database

--- app/src/main/java/com/example/pokergame/RegisterActivity.java
+++ app/src/main/java/com/example/pokergame/RegisterActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,9 +15,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -27,7 +34,8 @@ public class RegisterActivity extends AppCompatActivity {
     TextView loginBtnHolder;
     FirebaseAuth fAuth;
     ProgressBar progressBarHolder;
-
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,9 +51,10 @@ public class RegisterActivity extends AppCompatActivity {
         registerBtnHolder = findViewById(R.id.buttonRegister);
         loginBtnHolder = findViewById(R.id.alreadyRegistered);
 
-        //Starts up firebase database authenticator
-
+        //Starts up firebase database authenticator and FireStore database
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
         progressBarHolder = findViewById(R.id.progressBar);
         progressBarHolder.setVisibility(View.INVISIBLE);
 
@@ -108,6 +117,21 @@ public class RegisterActivity extends AppCompatActivity {
                    public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             Toast.makeText(RegisterActivity.this, "User Registered", Toast.LENGTH_SHORT).show();
+
+                            //Adds user name and email to database to reference
+                            userID = fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = fStore.collection("users").document(userID);
+                            Map<String, Object> user = new HashMap<>();
+
+                            user.put("username", userName);
+                            user.put("email", email);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Log.d("User Database", "onSuccess: user Profile is created for " + userID);
+                                }
+                            });
+
                             startActivity(new Intent(getApplicationContext(), MainActivity.class));
 
                         } else {
